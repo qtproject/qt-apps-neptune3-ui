qtproject/qt-apps-neptune3-ui

commit 5e83ee3430388041442aaeede6e54799dee552d6

[sysui] Load UI in two stages to get a first frame drawn faster
Change-Id: Id43b25e3549e05c503619df086596b3789a730ea
Reviewed-by: Lukáš Tinkl <ltinkl@luxoft.com>
Reviewed-by: Bramastyo Harimukti Santoso <bramastyo.harimukti.santoso@pelagicore.com>

sysui/display/Display.qml
@@ -41,6 +41,7 @@ import display 1.0
 import utils 1.0
 import animations 1.0
 import volume 1.0
+import statusbar 1.0
 
 import models.application 1.0
 import models.climate 1.0
@@ -56,6 +57,22 @@ import com.pelagicore.styles.neptune 3.0
 Item {
     id: root
 
+    /*
+        The UI is loaded in two steps
+        This is done in order to ensure that something is rendered on the screen as
+        soon as possible during start up.
+
+        Only the lightest elements are present upon creation of this component.
+        They are the ones that will be present on the very first rendered frame.
+
+        Others, which are more complex and thus take more time to load, will be
+        loaded afterwards, once this function is called.
+     */
+    function loadUI() {
+        applicationModel.populate();
+        mainContentArea.active = true;
+    }
+
     property Item popupParent
     property var settings
 
@@ -69,7 +86,7 @@ Item {
     Image {
         anchors.fill: parent
         source: Style.gfx(NeptuneStyle.backgroundImage)
-        opacity: launcherLoader.launcherOpen && NeptuneStyle.theme === NeptuneStyle.Light ? 0.7 : 1
+        opacity: mainContentArea.item && mainContentArea.item.launcherOpen && NeptuneStyle.theme === NeptuneStyle.Light ? 0.7 : 1
         Behavior on opacity { DefaultNumberAnimation {} }
     }
 
@@ -81,149 +98,25 @@ Item {
         id: volumeModel
     }
 
-    Instantiator {
-        model: applicationModel
-        delegate: QtObject {
-            property var exposedRectBottomMarginBinding: Binding {
-                target: model.appInfo
-                property: "exposedRectBottomMargin"
-                value: model.appInfo.active && widgetDrawer.open && widgetDrawer.visible
-                        ? activeApplicationSlot.height - widgetDrawer.y : climateBar.height
-            }
-
-            property var exposedRectTopMarginBinding: Binding {
-                target: model.appInfo
-                property: "exposedRectTopMargin"
-                value: model.appInfo.active ? launcherLoader.y + (launcherLoader.item.open ? NeptuneStyle.dp(Style.launcherHeight) : launcherLoader.height) : 0
-            }
-
-            property var windowHeightBinding: Binding {
-                target: model.appInfo.window
-                property: "height"
-                value: activeApplicationSlot.height
-            }
-        }
-    }
-
     // Content Elements
 
-    Item {
-        id: mainContentArea
-        anchors.left: parent.left
-        anchors.right: parent.right
-        height: root.height
-        opacity: launcherLoader.launcherOpen ? 0 : 1
-        Behavior on opacity { DefaultNumberAnimation {} }
-        enabled: !launcherLoader.launcherOpen
-        z: 0
-
-        Item {
-            y: launcherLoader.height - NeptuneStyle.dp(Style.launcherHeight)
-            width: parent.width
-            height: parent.height
-
-            StageLoader {
-                id: homePageLoader
-
-                anchors.left: parent.left
-                anchors.right: parent.right
-
-                y: launcherLoader.y + NeptuneStyle.dp(Style.launcherHeight)
-                height: parent.height - y - climateBar.height
-
-                active: true //StagedStartupModel.loadRest
-                source: "../home/HomePage.qml"
-                Binding { target: homePageLoader.item; property: "applicationModel"; value: applicationModel.populating ? null : applicationModel }
-
-                // widgets will reparent themselves to the active application slot when active
-                Binding { target: homePageLoader.item; property: "activeApplicationParent"; value: activeApplicationSlot }
-                Binding { target: homePageLoader.item; property: "moveBottomWidgetToDrawer"; value: !widgetDrawer.showingHomePage }
-                Binding { target: homePageLoader.item; property: "widgetDrawer"; value: widgetDrawerSlot }
-                Binding { target: homePageLoader.item; property: "popupParent"; value: popupParent }
-
-                property real homePageRowHeight: item ? item.rowHeight : 0
-            }
-
-            // slot for the maximized, active, application
-            Item {
-                id: activeApplicationSlot
-                anchors.fill: parent
-
-                ApplicationFrame {
-                    anchors.fill: parent
-                    appInfo: applicationModel.activeAppInfo && !applicationModel.activeAppInfo.asWidget ? applicationModel.activeAppInfo : null
-                }
-            }
-
-            WidgetDrawer {
-                id: widgetDrawer
-                width: parent.width
-                height: homePageLoader.homePageRowHeight
-                anchors.bottom: homePageLoader.bottom
-
-                dragEnabled: !showingHomePage
-                visible: !showingHomePage && !widgetDrawerSlot.empty
-
-                Item {
-                    id: widgetDrawerSlot
-                    width: widgetDrawer.homePageWidgetWidth
-                    height: homePageLoader.homePageRowHeight
-                    anchors.horizontalCenter: widgetDrawer.horizontalCenter
-                    readonly property bool empty: children.length == 0
-                }
-
-                property bool showingHomePage: applicationModel.activeAppInfo === null
-                onShowingHomePageChanged: {
-                    if (showingHomePage) {
-                        widgetDrawer.open = true;
-                    }
-                }
-
-                property real homePageWidgetWidth: homePageLoader.item ? homePageLoader.item.widgetWidth : 0
-            }
-        }
-    }
-
-    Rectangle {
-        id: mainContentMask
-        anchors.fill: mainContentArea
-        gradient: Gradient {
-            GradientStop { position: 0; color: Qt.rgba(1, 1, 1, 0.3) }
-            GradientStop { position: 0.8; color: Qt.rgba(1, 1, 1, 0.3) }
-            GradientStop { position: 0.95; color: Qt.rgba(1, 1, 1, 0) }
-        }
-        visible: false
-    }
-
-    OpacityMask {
-        anchors.fill: mainContentArea
-        source: mainContentArea
-        maskSource: mainContentMask
-    }
-
     StageLoader {
-        id: statusBarLoader
+        id: mainContentArea
+        source: "MainContentArea.qml"
+        anchors.fill: parent
+
+        Binding { target: mainContentArea.item; property: "applicationModel"; value: root.applicationModel }
+        Binding { target: mainContentArea.item; property: "launcherY"; value: statusBar.y + statusBar.height }
+        Binding { target: mainContentArea.item; property: "homeBottomMargin"; value: climateBar.height }
+    }
+
+    StatusBar {
+        id: statusBar
         height: NeptuneStyle.dp(Style.statusBarHeight)
         anchors.top: parent.top
         anchors.left: parent.left
         anchors.right: parent.right
-        active: StagedStartupModel.loadDisplay
-        source: "../statusbar/StatusBar.qml"
-        Binding { target: statusBarLoader.item; property: "uiSettings"; value: settings }
-        z: 1
-    }
-
-    StageLoader {
-        id: launcherLoader
-        property bool launcherOpen: launcherLoader.item ? launcherLoader.item.open : false
-        anchors.top: statusBarLoader.bottom
-        anchors.left: parent.left
-        anchors.right: parent.right
-        height: launcherLoader.item ? launcherLoader.item.height : NeptuneStyle.dp(Style.launcherHeight)
-        active: StagedStartupModel.loadDisplay
-        source: "../launcher/Launcher.qml"
-        Binding { target: launcherLoader.item; property: "applicationModel"; value: applicationModel }
-        Binding { target: launcherLoader.item; property: "showDevApps"; value: ApplicationManager.systemProperties.devMode }
+        uiSettings: settings
         z: 1
     }
 
@@ -315,9 +208,5 @@ Item {
         anchors.left: parent.left
         anchors.right: parent.right
         anchors.bottom: parent.bottom
-    }
-
-    Component.onCompleted: {
-        StagedStartupModel.enterMenuState()
     }
 }

Main.qml
@@ -54,6 +54,24 @@ Window {
     readonly property real smallerDimension: isLandscape ? height : width
     readonly property real largerDimension: isLandscape ? width : height
 
+    // Load the full UI once a frame has been drawn with the ligth version of it
+    // The very first frame is a blank. Only the second one has actual content,
+    // hence the counter.
+    // FIXME: Find a better way of finding out when the first proper frame has been
+    //       rendered (scene no longer dirty. render thread idle)
+    Connections {
+        id: windowConns
+        property int frameCount: 0
+        target: root
+        onFrameSwapped: {
+            frameCount = frameCount + 1;
+            if (frameCount === 2) {
+                display.loadUI();
+                windowConns.enabled = false;
+            }
+        }
+    }
+
     Display {
         id: display
 

imports/system/models/application/ApplicationModel.qml
@@ -56,6 +56,14 @@ ListModel {
     // Whether the model is still being populated. It's true during start up.
     readonly property bool populating: d.populating
 
+    // Populate the model
+    function populate() {
+        // Configures which applications should be shown as widgets,
+        // which, in turn, will cause them to be started.
+        d.configureApps();
+        d.populating = false;
+    }
+
     // Returns an ApplicationInfo given its index
     function application(index) {
         return get(index).appInfo;
@@ -167,8 +175,6 @@ ListModel {
             var app = AM.ApplicationManager.application(i);
             d.appendApplication(app);
         }
-        d.configureApps();
-        d.populating = false;
     }
 
     property var appManConns: Connections {

imports/system/models/startup/StagedStartupModel.qml
@@ -1,81 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2017 Pelagicore AG
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the Neptune 3 IVI UI.
-**
-** $QT_BEGIN_LICENSE:GPL-QTAS$
-** Commercial License Usage
-** Licensees holding valid commercial Qt Automotive Suite licenses may use
-** this file in accordance with the commercial license agreement provided
-** with the Software or, alternatively, in accordance with the terms
-** contained in a written agreement between you and The Qt Company.  For
-** licensing terms and conditions see https://www.qt.io/terms-conditions.
-** For further information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 or (at your option) any later version
-** approved by the KDE Free Qt Foundation. The licenses are as published by
-** the Free Software Foundation and appearing in the file LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-** SPDX-License-Identifier: GPL-3.0
-**
-****************************************************************************/
-
-pragma Singleton
-import QtQuick 2.0
-import QtQml.StateMachine 1.0 as DSM
-
-Item {
-    id: root
-
-    property bool loadDisplay: false
-    property bool loadRest: false
-    property bool loadBackgroundElements: false
-
-    signal enterMenuState()
-    signal enterFinalState()
-
-    DSM.StateMachine {
-        id: stateMachine
-        initialState: homePage
-        running: true
-
-        DSM.State {
-            id: homePage
-
-            DSM.SignalTransition {
-                targetState: menuState
-                signal: root.enterMenuState
-            }
-
-            onEntered: root.loadDisplay = true
-        }
-
-        DSM.State {
-            id: menuState
-
-            DSM.SignalTransition {
-                targetState: finalState
-                signal: root.enterFinalState
-            }
-
-            onEntered: root.loadRest = true
-        }
-
-        DSM.FinalState {
-            id: finalState
-
-            onEntered: root.loadBackgroundElements = true
-        }
-    }
-
-}
-

imports/system/models/startup/qmldir
@@ -1,1 +1,0 @@
-singleton StagedStartupModel 1.0 StagedStartupModel.qml

sysui/display/MainContentArea.qml
@@ -1,0 +1,177 @@
+/****************************************************************************
+**
+** Copyright (C) 2018 Pelagicore AG
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Neptune 3 IVI UI.
+**
+** $QT_BEGIN_LICENSE:GPL-QTAS$
+** Commercial License Usage
+** Licensees holding valid commercial Qt Automotive Suite licenses may use
+** this file in accordance with the commercial license agreement provided
+** with the Software or, alternatively, in accordance with the terms
+** contained in a written agreement between you and The Qt Company.  For
+** licensing terms and conditions see https://www.qt.io/terms-conditions.
+** For further information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 or (at your option) any later version
+** approved by the KDE Free Qt Foundation. The licenses are as published by
+** the Free Software Foundation and appearing in the file LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+** SPDX-License-Identifier: GPL-3.0
+**
+****************************************************************************/
+
+import QtQuick 2.10
+import QtGraphicalEffects 1.0
+
+import animations 1.0
+import home 1.0
+import launcher 1.0
+import utils 1.0
+
+import com.pelagicore.styles.neptune 3.0
+
+import QtApplicationManager 1.0
+
+Item {
+    id: root
+
+    property var applicationModel
+    property alias launcherY: launcher.y
+    property alias launcherOpen: launcher.open
+    property real homeBottomMargin
+
+    Instantiator {
+        model: root.applicationModel
+        delegate: QtObject {
+            property var exposedRectBottomMarginBinding: Binding {
+                target: model.appInfo
+                property: "exposedRectBottomMargin"
+                value: model.appInfo.active && widgetDrawer.open && widgetDrawer.visible
+                        ? activeApplicationSlot.height - widgetDrawer.y
+                        : root.homeBottomMargin
+            }
+
+            property var exposedRectTopMarginBinding: Binding {
+                target: model.appInfo
+                property: "exposedRectTopMargin"
+                value: model.appInfo.active ? launcher.y + (launcher.open ? NeptuneStyle.dp(Style.launcherHeight)
+                                                                          : launcher.height)
+                                            : 0
+            }
+
+            property var windowHeightBinding: Binding {
+                target: model.appInfo.window
+                property: "height"
+                value: activeApplicationSlot.height
+            }
+        }
+    }
+
+    Item {
+        id: mainContentArea
+        anchors.left: parent.left
+        anchors.right: parent.right
+        height: root.height
+        opacity: launcher.open ? 0 : 1
+        Behavior on opacity { DefaultNumberAnimation {} }
+        enabled: !launcher.open
+        z: 0
+
+        Item {
+            y: launcher.height - NeptuneStyle.dp(Style.launcherHeight)
+            width: parent.width
+            height: parent.height
+
+            HomePage {
+                id: homePage
+
+                anchors.left: parent.left
+                anchors.right: parent.right
+
+                y: launcher.y + NeptuneStyle.dp(Style.launcherHeight)
+                height: parent.height - y - root.homeBottomMargin
+
+                applicationModel: !root.applicationModel || root.applicationModel.populating ? null : root.applicationModel
+
+                // widgets will reparent themselves to the active application slot when active
+                activeApplicationParent: activeApplicationSlot
+                moveBottomWidgetToDrawer: !widgetDrawer.showingHomePage
+                widgetDrawer: widgetDrawerSlot
+                popupParent: popupParent
+            }
+
+            // slot for the maximized, active, application
+            Item {
+                id: activeApplicationSlot
+                anchors.fill: parent
+
+                ApplicationFrame {
+                    anchors.fill: parent
+                    appInfo: root.applicationModel && root.applicationModel.activeAppInfo
+                             && !root.applicationModel.activeAppInfo.asWidget ? root.applicationModel.activeAppInfo
+                                                                              : null
+                }
+            }
+
+            WidgetDrawer {
+                id: widgetDrawer
+                width: parent.width
+                height: homePage.rowHeight
+                anchors.bottom: homePage.bottom
+
+                dragEnabled: !showingHomePage
+                visible: !showingHomePage && !widgetDrawerSlot.empty
+
+                Item {
+                    id: widgetDrawerSlot
+                    width: homePage.widgetWidth
+                    height: homePage.rowHeight
+                    anchors.horizontalCenter: widgetDrawer.horizontalCenter
+                    readonly property bool empty: children.length == 0
+                }
+
+                property bool showingHomePage: !root.applicationModel || root.applicationModel.activeAppInfo === null
+                onShowingHomePageChanged: {
+                    if (showingHomePage) {
+                        widgetDrawer.open = true;
+                    }
+                }
+            }
+        }
+    }
+
+    Rectangle {
+        id: mainContentMask
+        anchors.fill: mainContentArea
+        gradient: Gradient {
+            GradientStop { position: 0; color: Qt.rgba(1, 1, 1, 0.3) }
+            GradientStop { position: 0.8; color: Qt.rgba(1, 1, 1, 0.3) }
+            GradientStop { position: 0.95; color: Qt.rgba(1, 1, 1, 0) }
+        }
+        visible: false
+    }
+
+    OpacityMask {
+        anchors.fill: mainContentArea
+        source: mainContentArea
+        maskSource: mainContentMask
+    }
+
+    Launcher {
+        id: launcher
+        anchors.left: parent.left
+        anchors.right: parent.right
+        applicationModel: root.applicationModel
+        showDevApps: ApplicationManager.systemProperties.devMode
+        z: 1
+    }
+}

sysui/display/qmldir
@@ -1,6 +1,7 @@
 ApplicationFrame 1.0 ApplicationFrame.qml
 CenterConsoleMonitorOverlay 1.0 CenterConsoleMonitorOverlay.qml
 Display 1.0 Display.qml
+MainContentArea 1.0 MainContentArea.qml
 ModalOverlay 1.0 ModalOverlay.qml
 WidgetDrawer 1.0 WidgetDrawer.qml
 VirtualKeyboard 1.0 VirtualKeyboard.qml

sysui/home/WidgetGrid.qml
@@ -323,7 +323,9 @@ Item {
                 readonly property real heightNormal: appInfo? appInfo.heightRows * root.rowHeight : 0
                 property real heightWhenResizing
 
-                property bool geometryBehaviorsEnabled: false
+                // Only enable geometry behaviors once WidgetGrid gets its size,
+                // otherwise widgets will animate from size (0,0) to their final sizes on startup.
+                property bool geometryBehaviorsEnabled: root.width > 0 && !widgetsList.populating
                 Behavior on y { enabled: geometryBehaviorsEnabled; DefaultSmoothedAnimation {} }
                 Behavior on height { enabled: geometryBehaviorsEnabled; DefaultSmoothedAnimation {} }
 
@@ -358,9 +360,6 @@ Item {
                         name: "normal"
                         PropertyChanges {
                             target: repeaterDelegate; y: yNormal; height: heightNormal
-                            // Only enable geometry behaviors once WidgetGrid gets its size,
-                            // otherwise widgets will animate from size (0,0) to their final sizes on startup.
-                            geometryBehaviorsEnabled: root.width > 0
                         }
                     },
                     State {

commit ab9af9a3c7cd81a232942d2883ece3ee05bbb499

[sysui] Delete import of a now absent module
Regression from 5e83ee3430388041442aaeede6e54799dee552d6

Change-Id: Ic3d93c40cad900723224c0125551cf7a4f7b451d
Reviewed-by: Lukáš Tinkl <ltinkl@luxoft.com>

sysui/display/Display.qml
@@ -46,7 +46,6 @@ import statusbar 1.0
 import models.application 1.0
 import models.climate 1.0
 import models.system 1.0
-import models.startup 1.0
 import models.volume 1.0
 import neptune.controls 1.0
 