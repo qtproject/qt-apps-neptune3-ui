qtproject/qt-apps-neptune3-ui

commit 40142235678d03bd21769022d87faa35d0f2daca

[sysui] remaining scaling fixes
Change-Id: I49371db31466c623caf46282f4e7a9049bca65a0
Reviewed-by: Daniel d'Andrada <daniel.dandrada@luxoft.com>
Reviewed-by: Bramastyo Harimukti Santoso <bramastyo.harimukti.santoso@pelagicore.com>

sysui/procmon/MonitorView.qml
@@ -74,24 +74,24 @@ ColumnLayout {
 
     StartupInfo {
         Layout.fillWidth: true
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.topMargin: NeptuneStyle.dp(20)
     }
 
     CpuMonitor {
         Layout.fillWidth: true
-        Layout.preferredHeight: Style.vspan(2.5)
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.preferredHeight: NeptuneStyle.dp(20)
+        Layout.topMargin: NeptuneStyle.dp(20)
     }
 
     RamMonitor {
         Layout.fillWidth: true
-        Layout.preferredHeight: Style.vspan(2.5)
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.preferredHeight: NeptuneStyle.dp(20)
+        Layout.topMargin: NeptuneStyle.dp(20)
     }
 
     NetworkMonitor {
         Layout.fillWidth: true
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.topMargin: NeptuneStyle.dp(20)
         addressList: info.addressList
         online: info.online
     }
@@ -100,7 +100,7 @@ ColumnLayout {
         text: qsTr("Version")
         Layout.fillWidth: true
         Layout.preferredHeight: font.pixelSize * 1.1
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.topMargin: NeptuneStyle.dp(20)
     }
     Label {
         text: Qt.application.version
@@ -114,7 +114,7 @@ ColumnLayout {
         text: qsTr("Platform")
         Layout.fillWidth: true
         Layout.preferredHeight: font.pixelSize * 1.1
-        Layout.topMargin: Style.vspan(0.25)
+        Layout.topMargin: NeptuneStyle.dp(20)
     }
     Label {
         text: info.productName + " · " + info.cpu + " · " + info.kernel + " · " +

styles/neptune/Slider.qml
@@ -77,6 +77,9 @@ T.Slider {
         y: control.topPadding +
            (control.horizontal ? (control.availableHeight - height) / 2 :
                                  control.visualPosition * (control.availableHeight - height))
+        width: NeptuneStyle.dp(sourceSize.width)
+        height: NeptuneStyle.dp(sourceSize.height)
+
         source: control.horizontal ?
                     Style.gfx("slider-handle-horizontal", NeptuneStyle.theme) :
                     Style.gfx("slider-handle-vertical", NeptuneStyle.theme)

sysui/climate/ClimatePopup.qml
@@ -137,7 +137,7 @@ NeptunePopup {
         anchors.top: parent.top
         anchors.topMargin: NeptuneStyle.dp(130) - leftTempSlider.handleHeight/2
         anchors.left: parent.left
-        anchors.leftMargin: -leftTempSlider.width/2 + NeptuneStyle.dp(30)/2
+        anchors.leftMargin: -leftTempSlider.width/2 + leftTempSlider.background.width/2
         height: NeptuneStyle.dp(1200)
         convertFunc: model.calculateUnitValue
 
@@ -168,7 +168,7 @@ NeptunePopup {
         anchors.top: parent.top
         anchors.topMargin: NeptuneStyle.dp(130) - rightTempSlider.handleHeight/2
         anchors.right: parent.right
-        anchors.rightMargin: -rightTempSlider.width/2 + NeptuneStyle.dp(30)/2
+        anchors.rightMargin: -rightTempSlider.width/2 + rightTempSlider.background.width/2
         height: NeptuneStyle.dp(1200)
         convertFunc: model.calculateUnitValue
 

sysui/display/Display.qml
@@ -122,7 +122,7 @@ Item {
     ClimateBar {
         id: climateBar
         width: root.width
-        height: Style.vspan(1.5)
+        height: NeptuneStyle.dp(120)
         anchors.bottom: parent.bottom
         popupParent: root.popupParent
         model: climateModel

sysui/display/WidgetDrawer.qml
@@ -31,6 +31,7 @@
 
 import QtQuick 2.6
 import utils 1.0
+import com.pelagicore.styles.neptune 3.0
 
 /*
   Displays a child item on the screen which can be dismissed by swiping it to the right.
@@ -62,7 +63,7 @@ MouseArea {
 
     // how much of it should be sticking out from the right side of the display when
     // the widget drawer has been closed
-    readonly property real stickOutWidth: Style.hspan(2)
+    readonly property real stickOutWidth: NeptuneStyle.dp(90)
 
     // true if in full view, false if it's tucked away on the right side of the display
     property bool open: true
@@ -99,7 +100,7 @@ MouseArea {
         property real lastX
         property int direction
         property bool dragging: false
-        readonly property real minimumDrag: Style.hspan(2)
+        readonly property real minimumDrag: NeptuneStyle.dp(90)
 
         function applyOpenState() {
             if (root.open) {
@@ -128,7 +129,7 @@ MouseArea {
     Behavior on x {
         SmoothedAnimation {
             easing.type: Easing.InOutQuad
-            velocity: Style.hspan(40)
+            velocity: NeptuneStyle.dp(1800)
         }
         enabled: !d.dragging
     }

sysui/procmon/MonitorPanel.qml
@@ -103,7 +103,7 @@ Item {
 
     Label {
         id: middleLineText
-        width: Style.hspan(1)
+        width: NeptuneStyle.dp(45)
         anchors.right: parent.right
         anchors.verticalCenter: graphContainer.verticalCenter
         font.pixelSize: NeptuneStyle.fontSizeS

sysui/volume/VolumePopup.qml
@@ -42,11 +42,11 @@ import controls 1.0
 NeptunePopup {
     id: root
 
-    width: Style.hspan(270/45)
-    height: Style.vspan(1426/80)
+    width: NeptuneStyle.dp(270)
+    height: NeptuneStyle.dp(1426)
 
     headerBackgroundVisible: true
-    headerBackgroundHeight: Style.vspan(140/80)
+    headerBackgroundHeight: NeptuneStyle.dp(140)
 
     readonly property alias value: slider.value
 
@@ -55,7 +55,7 @@ NeptunePopup {
     Row {
         id: labelVolumeValue
         anchors.top: parent.top
-        anchors.topMargin: Style.vspan(50/80)
+        anchors.topMargin: NeptuneStyle.dp(50)
         anchors.horizontalCenter: parent.horizontalCenter
         Label {
             anchors.verticalCenter: parent.verticalCenter
@@ -75,7 +75,7 @@ NeptunePopup {
     VolumeSlider {
         id: slider
         anchors.centerIn: parent
-        height: Style.vspan(14)
+        height: NeptuneStyle.dp(1120)
         from: 0
         to: 100
         value: root.model && root.model.muted ? 0 : Math.round(model.volume*100)
@@ -90,9 +90,9 @@ NeptunePopup {
         id: muteButton
         anchors.horizontalCenter: parent.horizontalCenter
         anchors.bottom: parent.bottom
-        anchors.bottomMargin: Style.vspan(40/80)
-        width: Style.hspan(200/45)
-        height: Style.vspan(100/80)
+        anchors.bottomMargin: NeptuneStyle.dp(40)
+        width: NeptuneStyle.dp(200)
+        height: NeptuneStyle.dp(100)
         checkable: true
         checked: root.model ? root.model.muted : false
         icon.name: "ic-volume-0"

sysui/volume/VolumeSlider.qml
@@ -32,11 +32,12 @@
 import QtQuick 2.6
 import QtQuick.Controls 2.2
 
+import com.pelagicore.styles.neptune 3.0
 import utils 1.0
 
 Slider {
     id: root
-    width: Style.hspan(4)
+    width: NeptuneStyle.dp(180)
 
     orientation: Qt.Vertical
     snapMode: Slider.SnapOnRelease

commit 8c5b0f3f077ef840d58265bcf052873c353c8c9c

[sysui] Fix height of graphs in About popup
Regression introduced by 40142235678d03bd21769022d87faa35d0f2daca

Change-Id: Ia659e87b589bb5c75fa1389f642f10f032def4a9
Reviewed-by: Bramastyo Harimukti Santoso <bramastyo.harimukti.santoso@pelagicore.com>
Reviewed-by: Lukáš Tinkl <ltinkl@luxoft.com>

sysui/procmon/MonitorView.qml
@@ -78,13 +78,13 @@ ColumnLayout {
 
     CpuMonitor {
         Layout.fillWidth: true
-        Layout.preferredHeight: NeptuneStyle.dp(20)
+        Layout.preferredHeight: NeptuneStyle.dp(200)
         Layout.topMargin: NeptuneStyle.dp(20)
     }
 
     RamMonitor {
         Layout.fillWidth: true
-        Layout.preferredHeight: NeptuneStyle.dp(20)
+        Layout.preferredHeight: NeptuneStyle.dp(200)
         Layout.topMargin: NeptuneStyle.dp(20)
     }
 